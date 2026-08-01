3dbe1f033499c9437766aa8f75b0076c831d0d96
jsppppwsh1 <goumriamel1@gmail.com>
2026-08-01 01:58:35 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,34 @@
-print("Ivy démarre...")
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f"Ivy est connectée en tant que {bot.user}")
+
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if message.content.lower().startswith("ivy"):
+        await message.channel.send(
+            "Tu viens de m'appeler ? J'espère que tu as une bonne raison 😈"
+        )
+
+    await bot.process_commands(message)
+
+
+bot.run(TOKEN)
